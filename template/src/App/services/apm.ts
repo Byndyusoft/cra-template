d504badf7a32bb465e2f6c2e77919b59c9c6917f
Eugene Abrosimov <hi@sadcitizen.me>
2020-12-23 11:58:25 +0500

    fix typescript issues

--- template/src/App/services/apm.ts
+++ template/src/App/services/apm.ts
@@ -1,6 +1,6 @@
 import { init } from '@elastic/apm-rum';
 
-export function setupApm() {
+export function setupApm(): void {
     init({
         serverUrl: process.env.REACT_APP_ELASTIC_APM_RUM_SERVER_URL,
         serviceName: process.env.REACT_APP_ELASTIC_APM_RUM_SERVICE_NAME,
